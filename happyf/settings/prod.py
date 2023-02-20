834aa6d7b7b3a5c4335ff09b5302120b2839c970
alexlee <353246407@qq.com>
2023-02-20 21:14:37 +0800

    配置requirements文件

--- happyf/settings/prod.py
+++ happyf/settings/prod.py
@@ -4,7 +4,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "happyf_db",
-        "USER": "root",
+        "USER": "root1",
         "PASSWORD": "wobuhuiwan",
         "HOST": "127.0.0.1",
         "PORT": "3306",

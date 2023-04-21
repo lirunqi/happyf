5cc1c0d57be366c2f2b2a07e713f1223525e2701
alexlee <353246407@qq.com>
2023-04-21 12:20:21 +0800

    dm1

--- dm1/form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class dm(forms.Form):
-    name = forms.CharField(max_length=200)
-    age = forms.IntegerField(max_value=1000)
-    sex = forms.CharField(max_length=3)
-    email = forms.EmailField()
---
+++ dmf/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DmfConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dmf'
